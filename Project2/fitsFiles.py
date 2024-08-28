55379c949ef0f0e4bc1120e3ad012cb4db8bef78
AJ-Shipp <anthonyjamesshipp@gmail.com>
2024-08-28 08:37:46 -0500

    Progress Update
    3-5:
    - Imported matplotlib and csv
    
    7-10:
    - [7/9] Added file paths for data file, and for my REU data file
    - [10] Variable for what file path the viewer will use
    
    12:
    - Creates blank image array (1024x2048)
      (Although it actually creates a square (2048x2048) array)
    
    14-22:
    - Opens data file
      - Appends each row to dataList variable
      - Inputs the data markers into the img   array
    
    24-25:
    - Prints out the first 5 things in the dataList array
    
    29:
    - Outputs the image info from the image into the myData variable
    
    31-35:
    - [31-32] Stores the image data from myData file into the image_data variable
    - [33-35] Plots the image from the image_data

--- Project2/fitsFiles.py
+++ Project2/fitsFiles.py
@@ -1,13 +1,37 @@
 import numpy as np
 from astropy.io import fits
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import csv
 
+dataFile = "C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/Project2/throughFocusProgram/dataOutPut.csv"
 fits_image = "C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/Project2/fitsExamples/proc_focus-1_mask_5sec.fits"
+myData = "myData.fits"
+inputImg = myData
+
+img = np.zeros((1024,2048))
+
+with open(dataFile, 'r') as csvfile:
+    csvReader = csv.reader(csvfile)
+
+    dataList = []
+    for row in csvReader:
+        dataList.append(row)
+    for row in dataList:
+        r = row
+        img[int(r[0])][int(r[1])] = int(r[2])        
+
+print(dataList[0:5])
+print("\n")
 
 with fits.open(fits_image) as hdul:
     hdul.info()
-    print(hdul._data)
-    data = hdul._data
-data[30:40, 10:20]
+    fits.writeto(myData, data=img, overwrite = True)
 
+image_data = fits.getdata(inputImg)
+print("\n")
+plt.imshow(image_data, cmap='gray')
+plt.colorbar()
+plt.show()
 
+print(img)
