593c93f8bf877f3a03ff48911f4416a07df4c362
AJ-Shipp <anthonyjamesshipp@gmail.com>
2024-08-30 11:54:04 -0500

    New m5 mm Image
    9:
    - New image file
    
    13:
    - Matching SLTF square detector image
    
    31:
    - Isolated this function call
    
    35:
    - Flipped Y-Axis values

--- Project2/fitsFiles.py
+++ Project2/fitsFiles.py
@@ -6,11 +6,11 @@ import csv
 
 #"C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/arc_m5.csv"
 dataFile = "C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/arc_m5.csv"
-fits_image = "C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/Project2/fitsExamples/proc_focus-1_mask_5sec.fits"
+fits_image = "C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/Project2/fitsExamples/proc_focus-5_mask_5sec.fits"
 myData = "myData+5.fits"
 inputImg = myData
 
-img = np.zeros((1024,2048))
+img = np.zeros((2048,2048))
 
 with open(dataFile, 'r') as csvfile:
     csvReader = csv.reader(csvfile)
@@ -27,12 +27,12 @@ print("\n")
 
 with fits.open(fits_image) as hdul:
     hdul.info()
-    fits.writeto(myData, data=img, overwrite = True)
+    
+fits.writeto(myData, data=img, overwrite = True)
 
 image_data = fits.getdata(inputImg)
 print("\n")
-plt.imshow(image_data, cmap='gray')
-#: plt.gca().invert_yaxis()
+plt.imshow(image_data, cmap='gray', origin='lower')
 plt.colorbar()
 #: plt.xlim()
 #: plt.ylim()
