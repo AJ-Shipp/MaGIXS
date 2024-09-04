64aaa60a40e401f4a519c77ab6d645c7aeebd46d
AJ-Shipp <anthonyjamesshipp@gmail.com>
2024-09-04 09:01:18 -0500

    REU Poster Adjustments
    6:
    - New matplotlib library import
    
    11-14:
    - [11] Changed output fits file
    - [12] Changed input fits file
    - [13] Created data rotation Boolean
    - [14] Created contour Boolean
    
    17:
    - Created theta variable for allowed mirror segment
    
    18:
    - Created rotation matrix to shift the .fits image
    
    26/29-42:
    - .fits image data rotation
    - xDiff & yDiff variables set the x/y offsets to align the center of the images
    - Parses through each (x,y) pair of data points, applies the rotation matrix, then replaces the original (x,y) pair with the components' rotated values in the data list
    
    44:
    - Prints out all values in the dataList
    
    54:
    - Changed image color mapping to jet from gray
    
    58-68:
    - When the contour Boolean is true, it plots the data's contour with number of levels equal to [value]

--- Project2/fitsFiles.py
+++ Project2/fitsFiles.py
@@ -3,14 +3,19 @@ from astropy.io import fits
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import csv
+import matplotlib.cm as cm
 
 #"C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/arc_m5.csv"
 dataFile = "C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/arc_m5.csv"
 fits_image = "C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/Project2/fitsExamples/proc_focus-5_mask_5sec.fits"
-myData = "myData+5.fits"
-inputImg = myData
+myData = "myData_M5_rot+shift.fits"
+inputImg = fits_image
+datRot = False
+contour = True
 
 img = np.zeros((2048,2048))
+th = np.radians(25)
+rotMtrx = np.array([[np.cos(th),np.sin(th)],[-np.sin(th),np.cos(th)]])
 
 with open(dataFile, 'r') as csvfile:
     csvReader = csv.reader(csvfile)
@@ -18,11 +23,25 @@ with open(dataFile, 'r') as csvfile:
     dataList = []
     for row in csvReader:
         dataList.append(row)
+
     for row in dataList:
         r = row
-        img[int(r[0])][int(r[1])] = int(r[2])        
+        xDiff = 0
+        yDiff = 0
+        if datRot == True:
+            xDiff = 393
+            yDiff = 549
 
-print(dataList[0:5])
+            x = float(r[0])
+            y = float(r[1])
+            oldPair = np.array([[x],[y]])
+            newPair = rotMtrx @ oldPair
+        
+            r[0] = int(np.floor(newPair[0][0]))
+            r[1] = int(np.floor(newPair[1][0]))
+        img[int(r[0])-xDiff][int(r[1])+yDiff] = float(r[2])/459*2600
+
+print(dataList[0:])
 print("\n")
 
 with fits.open(fits_image) as hdul:
@@ -32,11 +51,14 @@ fits.writeto(myData, data=img, overwrite = True)
 
 image_data = fits.getdata(inputImg)
 print("\n")
-plt.imshow(image_data, cmap='gray', origin='lower')
+plt.imshow(image_data, cmap='jet', origin='lower')
 plt.colorbar()
 #: plt.xlim()
 #: plt.ylim()
-plt.contour(image_data)
+
+if contour == True:
+    plt.contour(image_data, [1000])
+    
 plt.show()
 
 print(img)
