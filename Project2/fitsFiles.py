456779fdbab32090944fcbe15057f8f809bc9962
AJ-Shipp <anthonyjamesshipp@gmail.com>
2024-09-17 14:46:46 -0500

    Fits img rotation breakthrough
    10-12:
    - Changed file names
    
    13:
    - Created data rotation Boolean, and set it equal to True
    
    15:
    - Sets the image that is shown as being the SLTF image
    
    16:
    - Maximum pixel counts for the simulated image
    
    18-22:
    - Shows either the sim image or the SLTF image
    
    60:
    - Creates a second image data variable for the simulated data's contour
    
    64:
    - Plots the imageData file's image
    
    69-73:
    - Defines the image title
    - Plots the contour based on the simulated image data, with levels equaling 20, 40, 60, and 80, percent
    - Set's contour linewidth smaller
    - Plots image's colorbar
    - Plots contour's colorbar
    
    75-81:
    - Sets contour colorbar's labels (tick and title)
    - If else statement is the correct path, sets the image title and plots the colorbar of whatever image is sent through

--- Project2/fitsFiles.py
+++ Project2/fitsFiles.py
@@ -7,12 +7,19 @@ import matplotlib.cm as cm
 from matplotlib import colors
 
 #"C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/arc_m5.csv"
-dataFile = "C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/arc_m5.csv"
-fits_image = "C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/Project2/fitsExamples/proc_focus-5_mask_5sec.fits"
-myData = "myData_M5_rot+shift.fits"
-inputImg = fits_image
-datRot = False
+dataFile = "C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/Project2/simCsvs/arc_m4.csv"
+myData = "C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/Project2/simFits/myData_M4_rot+shift.fits"                #0
+fits_image = "C:/Users/antho/OneDrive/Documents/GitHub/MaGIXS/Project2/fitsExamples/proc_focus-4_mask_5sec.fits"    #1
+datRot = True
 contour = True
+dataSelect = 1
+simCountMax = 55
+
+if dataSelect == 0:
+    inputImg = myData
+    datRot = True
+elif dataSelect == 1:
+    inputImg = fits_image
 
 img = np.zeros((2048,2048))
 th = np.radians(25)
@@ -49,17 +56,30 @@ with fits.open(fits_image) as hdul:
     hdul.info()
     
 fits.writeto(myData, data=img, overwrite = True)
-
 image_data = fits.getdata(inputImg)
+image_data2 = fits.getdata(myData)
+
 print("\n")
-plt.imshow(image_data, cmap='jet', origin='lower', norm='linear', vmin=0)
-plt.colorbar()
+
+plot = plt.imshow(image_data, cmap='jet', origin='lower', norm='linear', vmin=0)
 plt.xlim(1000,1060)
 plt.ylim(960,1020)
 
 if contour == True:
-    plt.contour(image_data, [1000])
+    plot.axes.set_title('SLTF(m4) and csSim(m4)',fontsize=20)
+    cs = plt.contour(image_data2, levels=[simCountMax*1/5, simCountMax*2/5, simCountMax*3/5, simCountMax*4/5])
+    cs.set_linewidth(1)
+    cbc = plt.colorbar(cs, shrink=1)
+    cb = plt.colorbar(plot)
     
+    print(cbc.get_ticks())
+    cbc.set_ticks(ticks=cbc.get_ticks(),labels=['20%','40%','60%','80%'])
+    cbc.set_label('Percent of Max Intensity')
+else:
+    plot.axes.set_title('SLTF(m4)',fontsize=20)
+    plot.axes.set_title('csSim(m4)',fontsize=20)
+    cb = plt.colorbar(plot)
+
 plt.show()
 
-print(image_data)
+# print(image_data)
